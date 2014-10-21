04f29143411ad3698eadccb3d723e4f7973bd88b
Eric Fraze <magnilucent@gmail.com>
2014-10-20 21:30:04 -0400

    Added Page Background Grey

---
+++ Improvements/PageBackgroundGrey.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Page Background Grey
+// @version      0.1
+// @description  Changes the page background from white to grey
+// @author       Magnilucent
+// @match        https://www.mturk.com/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle("body { background-color: grey !important; }");
